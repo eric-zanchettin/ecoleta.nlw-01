f7f4573a84ce4c9043fc1971db0e59f273995b71
Eric Zanchettin Presto <canalmontrisz@hotmail.com>
2020-06-06 14:29:23 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+// JSX: Sintaxe do XML dentro do JavaScript
+
+import Routes from './routes';
+
+function App() {
+  return (
+      <Routes />
+  );
+}
+
+export default App;
